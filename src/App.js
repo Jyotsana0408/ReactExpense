a4911c895cfb55bd32b31b8805d24fa9f4462848
Jyotsana0408 <118711814+Jyotsana0408@users.noreply.github.com>
2023-07-14 11:26:28 +0530

    Add files via upload
    making a basic website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import ExpenceItem from './components/ExpenceItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ExpenceItem></ExpenceItem>
+    </div>
+  );
+}
+
+export default App;
